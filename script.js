c6cfe2992610c05f05ef5ca285bedfe4d805ca59
Gabriel K Aranda C <140840097+Gabriel90212@users.noreply.github.com>
2023-08-29 13:29:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var text = document.getElementById('text')
+var shadow = '';
+for(var i = 0; i < 30; i++)
+{
+    shadow +=(shadow? ',':'')+ -i*1+'px '+ i*1+ 'px 0 #01ded3';
+}
+text.style.textShadow = shadow;
